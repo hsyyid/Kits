fcfc256ad0684dccf8d9e8c685de9602347afa63
Hassan Syyid <hassansyyid@gmail.com>
2016-01-29 19:56:33 -0500

    Bug fixes

--- src/main/java/io/github/hsyyid/kits/cmds/KitExecutor.java
+++ src/main/java/io/github/hsyyid/kits/cmds/KitExecutor.java
@@ -2,7 +2,6 @@ package io.github.hsyyid.kits.cmds;
 
 import io.github.hsyyid.kits.utils.ConfigManager;
 import io.github.hsyyid.kits.utils.Utils;
-import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
@@ -10,11 +9,7 @@ import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.source.CommandBlockSource;
 import org.spongepowered.api.command.source.ConsoleSource;
 import org.spongepowered.api.command.spec.CommandExecutor;
-import org.spongepowered.api.data.DataContainer;
-import org.spongepowered.api.data.DataQuery;
 import org.spongepowered.api.entity.living.player.Player;
-import org.spongepowered.api.item.ItemType;
-import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.format.TextColors;
 
@@ -47,98 +42,13 @@ public class KitExecutor implements CommandExecutor
 
 				if (items.size() == 0)
 				{
-					player.sendMessage(Text.of(TextColors.RED, "Error: ", TextColors.DARK_RED, "The specified kit was not found, or there was an error retrieving data from it."));
+					player.sendMessage(Text.of(TextColors.RED, "Error! ", TextColors.DARK_RED, "The specified kit was not found, or there was an error retrieving data from it."));
 					return CommandResult.success();
 				}
 
 				if (ConfigManager.canUseKit(player.getUniqueId(), kit))
 				{
-					// Give Player their Kit
-					for (String i : items)
-					{
-						String id = i;
-						int quantity = 1;
-						int meta = -1;
-
-						if (id.contains(" "))
-						{
-							id = id.substring(0, id.indexOf(" "));
-							String substring = id.substring(id.indexOf(" ") + 1, id.length());
-
-							if (substring.contains(" "))
-							{
-								String quant = substring.substring(0, substring.indexOf(" "));
-
-								try
-								{
-									quantity = Integer.parseInt(quant);
-								}
-								catch (NumberFormatException e)
-								{
-									quantity = 1;
-								}
-
-								String met = substring.substring(substring.indexOf(" ") + 1, substring.length());
-
-								try
-								{
-									meta = Integer.parseInt(met);
-								}
-								catch (NumberFormatException e)
-								{
-									meta = -1;
-								}
-							}
-							else
-							{
-								try
-								{
-									quantity = Integer.parseInt(substring);
-								}
-								catch (NumberFormatException e)
-								{
-									quantity = 1;
-								}
-							}
-						}
-
-						Optional<ItemType> optionalItemType = Sponge.getRegistry().getType(ItemType.class, id);
-
-						if (optionalItemType.isPresent())
-						{
-							int c = 1;
-							
-							if (optionalItemType.get().getMaxStackQuantity() < quantity)
-							{
-								c = quantity;
-								quantity = 1;
-							}
-
-							for (int z = 0; z < c; z++)
-							{
-								ItemStack stack = Sponge.getRegistry()
-									.createBuilder(ItemStack.Builder.class)
-									.itemType(optionalItemType.get())
-									.quantity(quantity)
-									.build();
-
-								if (meta == -1)
-								{
-									player.getInventory().offer(stack);
-								}
-								else
-								{
-									DataContainer container = stack.toContainer().set(DataQuery.of("UnsafeDamage"), meta);
-									stack = Sponge.getRegistry()
-										.createBuilder(ItemStack.Builder.class)
-										.fromContainer(container)
-										.build();
-									player.getInventory().offer(stack);
-								}
-							}
-						}
-					}
-
+					Utils.givePlayerKit(player, items);
 					ConfigManager.setFalse(player.getUniqueId(), kit);
 
 					if (ConfigManager.getInterval(kit) instanceof Integer)
@@ -177,80 +87,7 @@ public class KitExecutor implements CommandExecutor
 		{
 			Player player = target.get();
 			List<String> items = ConfigManager.getItems(kit);
-
-			for (String i : items)
-			{
-				String id = i;
-				int quantity = 1;
-				int meta = -1;
-
-				if (id.contains(" "))
-				{
-					id = id.substring(0, id.indexOf(" "));
-					String substring = id.substring(id.indexOf(" ") + 1, id.length());
-
-					if (substring.contains(" "))
-					{
-						String quant = substring.substring(0, substring.indexOf(" "));
-
-						try
-						{
-							quantity = Integer.parseInt(quant);
-						}
-						catch (NumberFormatException e)
-						{
-							quantity = 1;
-						}
-
-						String met = substring.substring(substring.indexOf(" ") + 1, substring.length());
-
-						try
-						{
-							meta = Integer.parseInt(met);
-						}
-						catch (NumberFormatException e)
-						{
-							meta = -1;
-						}
-					}
-					else
-					{
-						try
-						{
-							quantity = Integer.parseInt(substring);
-						}
-						catch (NumberFormatException e)
-						{
-							quantity = 1;
-						}
-					}
-				}
-
-				Optional<ItemType> optionalItemType = Sponge.getRegistry().getType(ItemType.class, id);
-
-				if (optionalItemType.isPresent())
-				{
-					ItemStack stack = Sponge.getRegistry()
-						.createBuilder(ItemStack.Builder.class)
-						.itemType(optionalItemType.get())
-						.quantity(quantity)
-						.build();
-
-					if (meta == -1)
-					{
-						player.getInventory().offer(stack);
-					}
-					else
-					{
-						DataContainer container = stack.toContainer().set(DataQuery.of("UnsafeDamage"), meta);
-						stack = Sponge.getRegistry()
-							.createBuilder(ItemStack.Builder.class)
-							.fromContainer(container)
-							.build();
-						player.getInventory().offer(stack);
-					}
-				}
-			}
+			Utils.givePlayerKit(player, items);
 		}
 
 		return CommandResult.success();
--- src/main/java/io/github/hsyyid/kits/utils/Utils.java
+++ src/main/java/io/github/hsyyid/kits/utils/Utils.java
@@ -1,8 +1,15 @@
 package io.github.hsyyid.kits.utils;
 
 import org.spongepowered.api.Sponge;
+import org.spongepowered.api.data.DataContainer;
+import org.spongepowered.api.data.DataQuery;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.item.ItemType;
+import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.scheduler.Task;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -58,6 +65,96 @@ public class Utils
 							Utils.scheduleUpdateTask(uuid, kitName);
 							Utils.scheduleValueChangeTask(uuid, kitName, timeRemaining);
 						}
+					}
+				}
+			}
+		}
+	}
+	
+
+	public static void givePlayerKit(Player player, List<String> items)
+	{
+		// Give Player their Kit
+		for (String i : items)
+		{
+			String id = i;
+			int quantity = 1;
+			int meta = -1;
+
+			if (id.contains(" "))
+			{
+				String substring = id.substring(id.indexOf(" ") + 1, id.length());
+				id = id.substring(0, id.indexOf(" "));
+				
+				if (substring.contains(" "))
+				{
+					String quant = substring.substring(0, substring.indexOf(" "));
+					
+					try
+					{
+						quantity = Integer.parseInt(quant);
+					}
+					catch (NumberFormatException e)
+					{
+						quantity = 1;
+					}
+
+					String met = substring.substring(substring.indexOf(" ") + 1, substring.length());
+
+					try
+					{
+						meta = Integer.parseInt(met);
+					}
+					catch (NumberFormatException e)
+					{
+						meta = -1;
+					}
+				}
+				else
+				{
+					try
+					{
+						quantity = Integer.parseInt(substring);
+					}
+					catch (NumberFormatException e)
+					{
+						quantity = 1;
+					}
+				}
+			}
+
+			Optional<ItemType> optionalItemType = Sponge.getRegistry().getType(ItemType.class, id);
+
+			if (optionalItemType.isPresent())
+			{
+				int c = 1;
+				
+				if (quantity > optionalItemType.get().getMaxStackQuantity())
+				{
+					c = quantity;
+					quantity = 1;
+				}
+				
+				for (int z = 0; z < c; z++)
+				{
+					ItemStack stack = Sponge.getRegistry()
+						.createBuilder(ItemStack.Builder.class)
+						.itemType(optionalItemType.get())
+						.quantity(quantity)
+						.build();
+
+					if (meta == -1)
+					{
+						player.getInventory().offer(stack);
+					}
+					else
+					{
+						DataContainer container = stack.toContainer().set(DataQuery.of("UnsafeDamage"), meta);
+						stack = Sponge.getRegistry()
+							.createBuilder(ItemStack.Builder.class)
+							.fromContainer(container)
+							.build();
+						player.getInventory().offer(stack);
 					}
 				}
 			}
