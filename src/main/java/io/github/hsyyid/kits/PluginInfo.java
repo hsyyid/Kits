455b1f14e46f71173989b333c845239525791238
Hassan Syyid <hassansyyid@gmail.com>
2016-02-22 14:26:59 -0500

    bump version

--- src/main/java/io/github/hsyyid/kits/PluginInfo.java
+++ src/main/java/io/github/hsyyid/kits/PluginInfo.java
@@ -7,6 +7,6 @@ public abstract class PluginInfo
 {
 	public static final String ID = "Kits";
 	public static final String NAME = "Kits";
-	public static final String VERSION = "1.5c";
+	public static final String VERSION = "1.5d";
 	public static final String DEPENDENCIES = "after:Updatifier";
 }
